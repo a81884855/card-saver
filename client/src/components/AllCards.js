b5311ac4c92c59e06ea72832ed7eefd9c1ff337b
Gary <garyguan0713@gmail.com>
2019-09-27 16:53:43 -0700

    Implement Helmet

--- client/src/components/AllCards.js
+++ client/src/components/AllCards.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { getCardsQuery } from '../queries/queries.js';
+import { Helmet } from 'react-helmet';
+import { Col, Row, Image, Button } from 'react-bootstrap';
+
+const head = () => {
+  return (
+    <Helmet bodyAttributes={{ class: 'allCardsPage' }}>
+      <title>All Cards</title>
+    </Helmet>
+  );
+};
 
 export class CardList extends Component {
   constructor(props) {
@@ -16,23 +26,42 @@ export class CardList extends Component {
     } else {
       return data.Cards.map(card => {
         return (
-          <li
-            key={card.id}
-            onClick={e => {
-              this.setState({ selected: card.id });
-            }}
-          >
-            {card.name}
-          </li>
+          <Col md={4} style={{ marginBottom: '20px' }}>
+            <a href={`/cards/${card.name}`}>
+              <Image
+                src={`/images/${card.image}`}
+                style={{
+                  width: '100%'
+                }}
+                thumbnail
+              />
+              <p style={{ textAlign: 'center' }}>{card.name}</p>
+            </a>
+          </Col>
+
+          //   <a href={`/cards/${card.name}`}>{card.name}</a>
+          //   <Image
+          //     src={`/images/${card.image}`}
+          //     style={{
+          //       border: 'none',
+          //       background: 'none',
+          //       padding: 'none',
+          //       margin: '0 0 0 10px',
+          //       width: '88%'
+          //     }}
+          //     thumbnail
+          //   />
+          // </li>
         );
       });
     }
   }
   render() {
     return (
-      <div>
-        <ul id="book-list">{this.displayCards()}</ul>
-      </div>
+      <Row>
+        {head()}
+        {this.displayCards()}
+      </Row>
     );
   }
 }
