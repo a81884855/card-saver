cb20b9b34ae8b01ecb1f46c091bf4dd038277314
Gary <garyguan0713@gmail.com>
2019-09-27 16:38:30 -0700

    Implement Switch functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AllCards from './components/AllCards';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './layouts/layout';
 import Home from './components/Home';
 import Card from './components/Card';
@@ -9,32 +9,42 @@ import NotFound from './components/NotFound';
 function App() {
   return (
     <Router>
-      <Route
-        path="/"
-        exact
-        render={() => (
-          <Layout>
-            <Home />
-            {/* <AllCards /> */}
-          </Layout>
-        )}
-      />
-      <Route
-        path="/cards/:name"
-        render={props => (
-          <Layout>
-            <Card {...props} />
-          </Layout>
-        )}
-      />
-      <Route
-        path="/"
-        render={props => (
-          <Layout>
-            <NotFound {...props} />
-          </Layout>
-        )}
-      />
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <Layout>
+              <Home />
+              {/* <AllCards /> */}
+            </Layout>
+          )}
+        />
+        <Route
+          path="/cards/:name"
+          render={props => (
+            <Layout>
+              <Card {...props} />
+            </Layout>
+          )}
+        />
+        <Route
+          path="/cards"
+          exact
+          render={props => (
+            <Layout>
+              <AllCards {...props} />
+            </Layout>
+          )}
+        />
+        <Route
+          render={props => (
+            <Layout>
+              <NotFound {...props} />
+            </Layout>
+          )}
+        />
+      </Switch>
     </Router>
   );
 }
