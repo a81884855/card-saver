c93dacf5c3937691284bf347a7e470ab0874b832
Gary <garyguan0713@gmail.com>
2019-10-25 17:30:37 -0700

    Implement Modal to display card detail

--- client/src/components/AllCards.js
+++ client/src/components/AllCards.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { getCardsQuery } from '../queries/queries.js';
 import { Helmet } from 'react-helmet';
-import { Col, Row, Image } from 'react-bootstrap';
+import { Col, Row, Image, Modal, Button } from 'react-bootstrap';
+import CardDetail from './CardDetail';
+import 'animate.css/animate.min.css';
 
 const head = () => {
   return (
@@ -16,56 +18,111 @@ export class CardList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: null
+      show: false,
+      name: '',
+      website: ''
     };
+    this.handleShow = this.handleShow.bind(this);
   }
-  displayCards() {
-    let data = this.props.data;
+
+  handleShow = (name, website) => {
+    this.setState({
+      show: true,
+      name,
+      website
+    });
+  };
+
+  displayCards = props => {
+    let data = props.data;
     if (data.loading) {
-      return <div>Loading cards...</div>;
+      return <div>Loading card detail...</div>;
     } else {
       return data.Cards.map(card => {
-        return (
-          <Col md={4} key={card.id} style={{ marginBottom: '15px' }}>
-            <a href={`/cards/${card.name}`}>
-              <Image
-                src={`/images/${card.image}`}
-                style={{
-                  width: '100%',
-                  border: 'none'
-                }}
-                thumbnail
-              />
-              <p style={{ textAlign: 'center' }}>{card.name}</p>
-            </a>
-          </Col>
-
-          //   <a href={`/cards/${card.name}`}>{card.name}</a>
-          //   <Image
-          //     src={`/images/${card.image}`}
-          //     style={{
-          //       border: 'none',
-          //       background: 'none',
-          //       padding: 'none',
-          //       margin: '0 0 0 10px',
-          //       width: '88%'
-          //     }}
-          //     thumbnail
-          //   />
-          // </li>
-        );
+        return <Card key={card.id} card={card} handleShow={this.handleShow} />;
       });
     }
-  }
+  };
+
   render() {
+    const { show, name, website } = this.state;
     return (
       <>
+        <Modal
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          show={show}
+          onHide={() => this.setState({ show: false })}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>{name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <CardDetail name={name} />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => this.setState({ show: false })}>
+              Close
+            </Button>
+            <Button variant="primary" href={website}>
+              Learn More
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
         <h2>All Credit Cards</h2>
         <Row>
           {head()}
-          {this.displayCards()}
+          {this.displayCards(this.props)}
         </Row>
       </>
+    );
+  }
+}
+
+export class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovered: false
+    };
+    this.toggleHover = this.toggleHover.bind(this);
+  }
+  toggleHover() {
+    this.setState({
+      hovered: true
+    });
+    setTimeout(() => {
+      this.setState({
+        hovered: false
+      });
+    }, 1500);
+  }
+  render() {
+    const { card } = this.props;
+    const { hovered } = this.state;
+    return (
+      <Col
+        xs={10}
+        md={6}
+        lg={4}
+        key={card.id}
+        style={{ marginBottom: '15px' }}
+        onClick={() => this.props.handleShow(card.name, card.website)}
+      >
+        <Image
+          src={`/images/${card.image}`}
+          style={{
+            width: '100%',
+            border: 'none'
+          }}
+          onMouseEnter={this.toggleHover}
+          className={hovered ? 'flipInX animated' : ''}
+          thumbnail
+        />
+        <p style={{ textAlign: 'center' }}>{card.name}</p>
+      </Col>
     );
   }
 }
