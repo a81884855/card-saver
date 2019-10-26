dc1107fe1655295301d444e0314185254f439c51
Gary <garyguan0713@gmail.com>
2019-10-25 17:30:18 -0700

    Use displayIcon.js to reframing

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,36 +1,20 @@
-import React, { Component, useState } from 'react';
-import { Row, Col, Image, Container, Jumbotron } from 'react-bootstrap';
-import 'animate.css/animate.min.css';
+import React from 'react';
+import { Row, Col, Container, Jumbotron } from 'react-bootstrap';
 import ScrollAnimation from 'react-animate-on-scroll';
 import { graphql } from 'react-apollo';
 import { getCategoryQuery } from '../queries/queries.js';
+import displayIcon from '../assets/helper/displayIcon';
+import Card from './Card';
 
-import {
-  MdLocalGasStation,
-  MdRestaurant,
-  MdCardTravel,
-  MdComputer,
-  MdPhoneIphone,
-  MdLocalGroceryStore
-} from 'react-icons/md';
-import { TiHomeOutline } from 'react-icons/ti';
-import { GiCeilingLight } from 'react-icons/gi';
-import { IoIosDesktop } from 'react-icons/io';
-
-export class Category extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  displayCards() {
-    let { data, category } = this.props;
+export function Category(props) {
+  const displayCards = props => {
+    let { data, category } = props;
     if (data.loading) {
-      return <div>Loading cards...</div>;
+      return <></>;
     } else {
       return data.Cards.sort((a, b) => {
         if (b[`${category}`] === a[`${category}`]) {
-          return b.name.length - a.name.length;
+          return b[`${category}Additional`].length - a[`${category}Additional`].length;
         } else {
           return b[`${category}`] - a[`${category}`];
         }
@@ -44,45 +28,28 @@ export class Category extends Component {
           );
         });
     }
-  }
+  };
 
-  displayCategoryIcon() {
-    let { data, category } = this.props;
-    if (data.loading) {
-      return <div>...</div>;
-    } else {
-      if (category === 'gas') return <MdLocalGasStation />;
-      if (category === 'restaurant') return <MdRestaurant />;
-      if (category === 'travel') return <MdCardTravel />;
-      if (category === 'online') return <MdComputer />;
-      if (category === 'phone') return <MdPhoneIphone />;
-      if (category === 'grocery') return <MdLocalGroceryStore />;
-      if (category === 'furnitures') return <TiHomeOutline />;
-      if (category === 'streaming') return <IoIosDesktop />;
-      if (category === 'utilities') return <GiCeilingLight />;
-    }
-  }
-
-  displayCategoryHead() {
-    let { data, category } = this.props;
+  const displayCategoryHead = props => {
+    let { data, category } = props;
     if (data.loading) {
       return <div>Loading cards...</div>;
     } else {
-      const { Category } = this.props.data;
+      const { Category } = props.data;
       return (
         <Jumbotron style={{ padding: '1rem 1rem', marginTop: '1rem' }} fluid>
           <Container>
             <h3>
               <span style={{ fontSize: 'larger', margin: '-3px 3px' }}>
-                {this.displayCategoryIcon()}
+                {displayIcon(category)}
               </span>
               {category.charAt(0).toUpperCase() + category.slice(1)}
             </h3>
             <p>{Category.detail}</p>
             <p style={{ fontSize: '12px' }}>
               * Below is a list of popular merchants in this category. While the list can be a
-              helpful guide, we does not control how credit card publishers identify merchants or
-              purchases are categorized.
+              helpful guide, we does not control how the bank identify merchants or purchases are
+              categorized.
             </p>
             <Row>
               {Category.merchant.map(merchant => (
@@ -95,54 +62,15 @@ export class Category extends Component {
         </Jumbotron>
       );
     }
-  }
-
-  render() {
-    return (
-      <Container>
-        {this.displayCategoryHead()}
-        <ol>{this.displayCards()}</ol>
-      </Container>
-    );
-  }
+  };
+  return (
+    <Container>
+      {displayCategoryHead(props)}
+      <ol>{displayCards(props)}</ol>
+    </Container>
+  );
 }
 
-const Card = props => {
-  const [hovered, setHovered] = useState(false);
-  const toggleHover = () => setHovered(!hovered);
-  const { card, category } = props;
-  return (
-    <li
-      className={hovered ? 'pulse animated ' : ''}
-      style={{ marginBottom: '18px' }}
-      onMouseEnter={toggleHover}
-      onMouseLeave={toggleHover}
-    >
-      <a href={`/cards/${card.name}`}>
-        <div>
-          <p style={{ margin: 0 }}>
-            {card.name} - {card[`${category}`]}%
-            <span style={{ paddingLeft: '5px', color: 'hotpink', fontStyle: 'oblique' }}>
-              {card[`${category}Additional`] && '(' + card[`${category}Additional`] + ')'}
-            </span>
-          </p>
-          <Image
-            src={`/images/${card.image}`}
-            style={{
-              border: 'none',
-              background: 'none',
-              padding: 'none',
-              width: '80%'
-            }}
-            thumbnail
-          />
-        </div>
-      </a>
-    </li>
-  );
-};
-
-// export default Category;
 export default graphql(getCategoryQuery, {
   options: props => {
     return {
