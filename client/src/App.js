4ed2c4fa7872df860b89e92b0ee7b31504388002
Gary <garyguan0713@gmail.com>
2019-11-13 21:57:45 -0800

    Update Layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,58 +2,25 @@ import React from 'react';
 import AllCards from './components/AllCards';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './layouts/layout';
-import Categories from './components/Categories';
+import Categories from './components/Category';
 import NotFound from './components/NotFound';
 import Calculator from './components/Calculator/';
+import Home from './components/Home';
+import CardPicker from './components/CardPicker/';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route
-          path="/"
-          exact
-          render={() => (
-            <Layout>
-              <Categories />
-            </Layout>
-          )}
-        />
-        <Route
-          path="/category"
-          exact
-          render={() => (
-            <Layout>
-              <Categories />
-            </Layout>
-          )}
-        />
-        <Route
-          path="/cards"
-          exact
-          render={props => (
-            <Layout>
-              <AllCards {...props} />
-            </Layout>
-          )}
-        />
-        <Route
-          path="/calculator"
-          exact
-          render={props => (
-            <Layout>
-              <Calculator {...props} />
-            </Layout>
-          )}
-        />
-        <Route
-          render={props => (
-            <Layout>
-              <NotFound {...props} />
-            </Layout>
-          )}
-        />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact render={() => <Home />} />
+          <Route path="/category" exact render={() => <Categories />} />
+          <Route path="/cards" exact render={props => <AllCards {...props} />} />
+          <Route path="/calculator" exact render={props => <Calculator {...props} />} />
+          <Route path="/card-picker" exact render={props => <CardPicker {...props} />} />
+          <Route render={props => <NotFound {...props} />} />
+        </Switch>
+      </Layout>
     </Router>
   );
 }
