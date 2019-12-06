febc533ed098f3b6e355797e09ea437b5546904a
Gary <garyguan0713@gmail.com>
2019-12-06 13:27:59 -0800

    add marginTop -5 px for Navbar Brand

--- client/src/layouts/header.js
+++ client/src/layouts/header.js
@@ -6,7 +6,7 @@ export default function Header() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="/">
-          <Image src="/apple-touch-icon.png" style={{ width: '30px', marginRight: '5px' }} />
+          <Image src="/apple-touch-icon.png" style={{ width: '30px', margin: '-5px 5px' }} />
           Super Card Saver
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
