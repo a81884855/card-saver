f932f20bc95fe66ac507a9598978b08b2e9b0fca
Gary <garyguan0713@gmail.com>
2019-12-09 20:32:00 -0800

    Implement Card detail on result

--- client/src/components/CardAdvisor/Result.js
+++ client/src/components/CardAdvisor/Result.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { graphql } from 'react-apollo';
 import { getCardsQuery } from '../../queries/queries.js';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image, Modal, Button } from 'react-bootstrap';
 import { FaCrown } from 'react-icons/fa';
+import CardDetail from '../Category/CardDetail';
 
 export class Result extends Component {
   constructor(props) {
@@ -66,66 +67,93 @@ export class Result extends Component {
 
 export function Card(props) {
   const { card, category, info, reward } = props;
+  const [show, setShow] = useState(false);
+
   return (
     <>
       {card && (
-        <Col
-          xs={10}
-          lg={4}
-          key={card.id}
-          style={{ margin: '10px auto 30px' }}
-          onClick={() => this.props.handleShow(card.name, card.website)}
-        >
-          <h3 style={{ textTransform: 'capitalize', textAlign: 'center' }}>{category} Card</h3>
-          <Image
-            src={`/images/${card.image}`}
-            style={{
-              width: '100%',
-              border: 'none'
-            }}
-            thumbnail
-          />
-          <p style={{ textAlign: 'center', fontSize: '1.3rem', color: 'tomato' }}>{card.name}</p>
-          <Container style={{ textAlign: 'center' }}>
-            <Row style={{ fontWeight: 700 }}>
-              <Col xs={3}>Yours</Col>
-              <Col style={{ padding: 0 }} xs={{ span: 3, offset: 6 }}>
-                Suggested
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={3}>{info[`${category}Reward`]}</Col>
-              <Col xs={6}>% Reward</Col>
-              <Col xs={3}>{card[category]}</Col>
-            </Row>
-            <Row>
-              <Col xs={3}>{info[`${category}Annual`] | 0}</Col>
-              <Col xs={6}>Annual Fee</Col>
-              <Col xs={3}>{card.annual}</Col>
-            </Row>
-            <Row>
-              <Col xs={3}>
-                $
-                {(info[category] * info[`${category}Reward`] * 12) / 100 -
-                  info[`${category}Annual`]}
-                {reward <=
-                  (info[category] * info[`${category}Reward`] * 12) / 100 -
-                    info[`${category}Annual`] && (
-                  <FaCrown style={{ color: 'darkorange', margin: '-5px 0 0 0' }} />
-                )}
-              </Col>
-              <Col xs={6}>Annual Saving</Col>
-              <Col xs={3}>
-                ${Math.round(reward)}{' '}
-                {reward >=
-                  (info[category] * info[`${category}Reward`] * 12) / 100 -
-                    info[`${category}Annual`] && (
-                  <FaCrown style={{ color: 'darkorange', margin: '-5px 0 0 0' }} />
-                )}
-              </Col>
-            </Row>
-          </Container>
-        </Col>
+        <>
+          <Modal
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            show={show}
+            onHide={() => setShow(false)}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>{card.name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <CardDetail name={card.name} />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={() => setShow(false)}>
+                Close
+              </Button>
+              <Button variant="primary" href={card.website}>
+                Learn More
+              </Button>
+            </Modal.Footer>
+          </Modal>
+
+          <Col
+            xs={10}
+            lg={4}
+            key={card.id}
+            style={{ margin: '10px auto 30px' }}
+            onClick={() => setShow(true)}
+          >
+            <h3 style={{ textTransform: 'capitalize', textAlign: 'center' }}>{category} Card</h3>
+            <Image
+              src={`/images/${card.image}`}
+              style={{
+                width: '100%',
+                border: 'none'
+              }}
+              thumbnail
+            />
+            <p style={{ textAlign: 'center', fontSize: '1.3rem', color: 'tomato' }}>{card.name}</p>
+            <Container style={{ textAlign: 'center' }}>
+              <Row style={{ fontWeight: 700 }}>
+                <Col xs={3}>Yours</Col>
+                <Col style={{ padding: 0 }} xs={{ span: 3, offset: 6 }}>
+                  Suggested
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={3}>{info[`${category}Reward`]}</Col>
+                <Col xs={6}>% Reward</Col>
+                <Col xs={3}>{card[category]}</Col>
+              </Row>
+              <Row>
+                <Col xs={3}>{info[`${category}Annual`] | 0}</Col>
+                <Col xs={6}>Annual Fee</Col>
+                <Col xs={3}>{card.annual}</Col>
+              </Row>
+              <Row>
+                <Col xs={3}>
+                  $
+                  {(info[category] * info[`${category}Reward`] * 12) / 100 -
+                    info[`${category}Annual`]}
+                  {reward <=
+                    (info[category] * info[`${category}Reward`] * 12) / 100 -
+                      info[`${category}Annual`] && (
+                    <FaCrown style={{ color: 'darkorange', margin: '-5px 0 0 0' }} />
+                  )}
+                </Col>
+                <Col xs={6}>Annual Saving</Col>
+                <Col xs={3}>
+                  ${Math.round(reward)}{' '}
+                  {reward >=
+                    (info[category] * info[`${category}Reward`] * 12) / 100 -
+                      info[`${category}Annual`] && (
+                    <FaCrown style={{ color: 'darkorange', margin: '-5px 0 0 0' }} />
+                  )}
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+        </>
       )}
     </>
   );
