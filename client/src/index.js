d616e6dfa090353267053a15bfa9c57946cc2b49
Gary <garyguan0713@gmail.com>
2020-02-07 20:46:59 -0800

    use serverless endpoint

--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,8 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-  // uri: `${window.location.protocol}//${window.location.hostname}:4000/graphql`
   uri: 'https://kvbshnusjk.execute-api.us-east-1.amazonaws.com/dev/graphql'
+  // uri: 'http://localhost:3000/graphql'
 });
 
 ReactDOM.render(
